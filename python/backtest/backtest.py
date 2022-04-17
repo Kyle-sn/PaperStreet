52a7e42e3a8d457792a2abeaf34fdc8b22a4cb84
Kyle K <28989869+Kyle-sn@users.noreply.github.com>
2022-04-17 11:07:58 -0500

    add basic backtest framework using backtrader lib

---
+++ python/backtest/backtest.py
@@ -0,0 +1,138 @@
+''' Basic backtesting module '''
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import datetime
+import os.path
+import sys
+
+import backtrader as bt
+
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+    ''' Base class to be subclassed for user defined strategies. '''
+    params = (
+        ('maperiod', 33),
+    )
+
+    def log(self, txt, dt=None):
+        ''' Logging function for this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print(f'{dt.isoformat()}, {txt}')
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        # Add a MovingAverageSimple indicator
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.maperiod)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'BUY EXECUTED, Price: {order.executed.price}, Cost: {order.executed.value}, Comm {order.executed.comm}')
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log(f'SELL EXECUTED, Price: {order.executed.price}, Cost: {order.executed.value}, Comm {order.executed.comm}')
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Write down: no pending order
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log(f'OPERATION PROFIT, GROSS {trade.pnl}, NET {trade.pnlcomm}')
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log(f'Close, {self.dataclose[0]}')
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if self.dataclose[0] > self.sma[0]:
+
+                # Buy with all possible default parameters
+                self.log(f'BUY CREATE, {self.dataclose[0]}')
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
+
+        else:
+
+            if self.dataclose[0] < self.sma[0]:
+                # Sell with all possible default parameters
+                self.log(f'SELL CREATE, {self.dataclose[0]}')
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
+
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, 'C:/Users/kylek/Downloads/QQQ.csv')
+
+    # Create a Data Feed
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath,
+        # Do not pass values before this date
+        fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values before this date
+        todate=datetime.datetime(2022, 4, 16),
+        # Do not pass values after this date
+        reverse=False)
+
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(5000.0)
+
+    # Set the commission
+    cerebro.broker.setcommission(commission=0.001)
+
+    # Print out the starting conditions
+    print(f'Starting Portfolio Value: {cerebro.broker.getvalue()}')
+
+    # Run over everything
+    cerebro.run()
+
+    # Print out the final result
+    print(f'Final Portfolio Value: {cerebro.broker.getvalue()}')
+
+    # Plot the result
+    cerebro.plot()
+    
