205d7889ab65de4c3f7c82eaa89d23e8072f59f3
Kyle K <28989869+Kyle-sn@users.noreply.github.com>
2022-05-03 18:13:46 -0500

    use environment variable for account number in Trader.java

--- src/main/java/com/paperstreet/strategy/Trader.java
+++ src/main/java/com/paperstreet/strategy/Trader.java
@@ -3,20 +3,23 @@ package com.paperstreet.strategy;
 import com.paperstreet.linehandler.OrderHandler;
 import com.paperstreet.positionhandler.PositionHandler;
 
+import java.util.concurrent.TimeUnit;
+
 public class Trader {
 
     public static final OrderHandler orderHandler = new OrderHandler();
     public static final PositionHandler positionHandler = new PositionHandler();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // start up the position handler
         positionHandler.connectPositionHandler();
-        positionHandler.requestAccountUpdates(true, "DU5231415");
-        positionHandler.requestAccountSummary(6, "ALL");
+        positionHandler.requestAccountUpdates(true, System.getenv("ACCOUNT_NUMBER"));
+        positionHandler.requestAccountSummary(7, "ALL");
         positionHandler.requestPositions();
 
         // start up the order handler
         orderHandler.connectOrderHandler();
+        TimeUnit.SECONDS.sleep(5);
         orderHandler.sendMarketOrder("QQQ", "BUY", 100);
     }
 }
