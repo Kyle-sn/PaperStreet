5704c8ac1f0d47ef0da259e6f6cb48f7b356cd9c
kyle <200~28989869+Kyle-sn@users.noreply.github.com>
2024-05-21 21:59:49 -0500

    Finalize basic framework for backtest.py and strategy.py

--- python/backtest/backtest.py
+++ python/backtest/backtest.py
@@ -1,30 +1,31 @@
-''' Basic backtesting module. '''
+""" Basic backtesting module. """
 
 import os
 import sys
 import backtrader as bt
 from strategy import TestStrategy
 
+
 class BackTest:
-    ''' Class used to run a backtest on a specified strategy. '''
+    """ Class used to run a backtest on a specified strategy. """
 
     def __init__(self, strategy):
         self.strategy = strategy
 
     def run(self):
-        ''' Run backtest. '''
+        """ Run backtest. """
 
         cerebro = bt.Cerebro()
-
         cerebro.addstrategy(self.strategy)
 
         modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-        datapath = os.path.join(modpath, 'C:/Users/kylek/Downloads/QQQ.csv')
+        datapath = os.path.join(modpath, 'C:/Users/kylek/data/market_data/yahoo_finance/QQQ_raw_test.csv')
 
         # Create a Data Feed
+        #TODO: implement GenericCSVData instead of YahooFinanceCSVData
+        # need to figure out how the normalized data should look first to make sure it works with this parser
         data = bt.feeds.YahooFinanceCSVData(dataname=datapath, reverse=False)
 
-        # Add the Data Feed to Cerebro
         cerebro.adddata(data)
 
         cerebro.broker.setcash(5000.0)
@@ -32,21 +33,17 @@ class BackTest:
 
         start_portfolio_value = cerebro.broker.getvalue()
 
-        # Run Cerebro Engine
         cerebro.run()
 
         end_portfolio_value = cerebro.broker.getvalue()
         pnl = end_portfolio_value - start_portfolio_value
 
-        # Print out the final result
         print(f'Final Portfolio Value: {cerebro.broker.getvalue()}')
         print(f'PnL: {pnl}')
 
-        # Plot the result
         cerebro.plot()
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     backtest = BackTest(TestStrategy)
     backtest.run()
-    
--- python/backtest/strategy.py
+++ python/backtest/strategy.py
@@ -1,78 +1,73 @@
-''' Strategy to be backtested. '''
+""" Strategy to be backtested. """
 
 import backtrader as bt
 
-# Create a Stratey
-class TestStrategy(bt.Strategy):
-    ''' Base class to be subclassed for user defined strategies. '''
 
-    # Moving average parameters
-    params = (('pfast',2),('pslow',184),)
+# Create a Strategy
+class TestStrategy(bt.Strategy):
+    """ Base class to be subclassed for user defined strategies. """
 
     def __init__(self):
 
-        self.dataclose = self.datas[0].close
-        self.datahigh = self.datas[0].high
-        self.datalow = self.datas[0].low
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.data_close = self.datas[0].close
 
-		# Order variable will contain ongoing order details/status
+        # To keep track of pending orders
         self.order = None
-
-        # Instantiate moving averages
-        self.slow_sma = bt.indicators.MovingAverageSimple(self.datas[0], 
-                        period=self.params.pslow)
-        self.fast_sma = bt.indicators.MovingAverageSimple(self.datas[0], 
-                        period=self.params.pfast)
 
         self.bar_executed = 0
 
     def log(self, txt, dt=None):
-        ''' Logging function for this strategy. '''
+        """ Logging function for this strategy. """
 
         dt = dt or self.datas[0].datetime.date(0)
         print(f'{dt.isoformat()}, {txt}')
 
-    def next(self):
-        '''
-        This method will be called for all remaining data points when
-        the minimum period for all datas/indicators have been meet.
-        '''
-
-        # Check for open orders
-        if self.order:
-            return
-
-        # Check if we are in the market
-        if not self.position:
-            # We are not in the market, look for a signal to OPEN trades
-
-            if self.fast_sma[0] > self.slow_sma[0]:
-                self.log(f'BUY CREATED: {self.dataclose[0]:2f}')
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.buy()
-            elif self.fast_sma[0] < self.slow_sma[0]:
-                self.log(f'SELL CREATED: {self.dataclose[0]}')
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.sell()
-
     def notify_order(self, order):
-        ''' Receives an order whenever there has been a change in one. '''
+        """ Receives an order whenever there has been a change in one. """
 
         if order.status in [order.Submitted, order.Accepted]:
             # An active Buy/Sell order has been submitted/accepted - Nothing to do
             return
 
         # Check if an order has been completed
-        # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log(f'BUY EXECUTED: {order.executed.price}')
+                self.log(f'Buy executed @ {order.executed.price}')
             elif order.issell():
-                self.log(f'SELL EXECUTED: {order.executed.price}')
-            self.bar_executed = len(self)
+                self.log(f'Sell executed @  {order.executed.price}')
 
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log('Order Canceled/Margin/Rejected')
 
         # Reset orders
         self.order = None
+
+    def next(self):
+        """
+        This method will be called for all remaining data points when
+        the minimum period for all datas/indicators have been meet.
+        """
+
+        self.log(f'Close:  {self.data_close[0]}')
+
+        # check for open orders
+        if self.order:
+            return
+
+        if not self.position:
+            if self.data_close[0] < self.data_close[-3]:
+                if self.data_close[-1] < self.data_close[-4]:
+
+                    self.log(f'Buy submitted @ {self.data_close[0]}')
+
+                    # keep track of the created order to avoid a 2nd order
+                    self.order = self.buy()
+
+        else:
+            if self.data_close[0] > self.data_close[-5]:
+                self.log(f'Sell submitted @ {self.data_close[0]}')
+
+                # keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
