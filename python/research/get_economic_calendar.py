5c4dd3867bafe1c5187c4595cd3ff4a539c5955a
Kyle Kauss <28989869+Kyle-sn@users.noreply.github.com>
2024-11-02 11:51:53 -0500

    add logic to import economic_releases into db

--- python/research/get_economic_calendar.py
+++ python/research/get_economic_calendar.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import pandas as pd
+import mysql.connector
+import os
 
 
 def process_data():
@@ -12,18 +14,22 @@ def process_data():
         json.dump(data["release_dates"], file)
 
     df = pd.read_json(json_file_path)
-    df.drop(['release_id', 'release_last_updated'], axis=1, inplace=True)
     df.to_csv(csv_file_path, index=False)
+
+    insert_data_to_db(csv_file_path)
 
 
 def get_data():
-    # Get release dates for all releases of economic data.
+    # get release dates for all releases of economic data.
     url_base = 'https://api.stlouisfed.org/fred/releases/dates?'
 
+    # keeping the realtime start/end dates as the latest date will make it so that
+    # the csv being written only has that data. Downstream this allows for only the
+    # latest day's data to be inserted into the db.
     url_params = {
         'file_type': 'json',
-        'realtime_start': '2024-08-04',
-        'realtime_end': '2024-08-31',
+        'realtime_start': '2024-11-01',
+        'realtime_end': '2024-11-01',
         'api_key': '48364fda9bc48ee78d182b9b58bee35b',
         'include_release_dates_with_no_data': 'true',
     }
@@ -35,6 +41,43 @@ def get_data():
             f'Authentication error: status code {response.status_code}, reason: {response.reason}')
 
     return json.loads(response.content)
+
+
+def insert_data_to_db(csv_file_path):
+    data = pd.read_csv(csv_file_path)
+    # convert the 'release_last_updated' column to DATE format (YYYY-MM-DD)
+    data['release_last_updated'] = pd.to_datetime(data['release_last_updated']).dt.date
+    
+    # create a new concatenated column to serve as the primary key in the db
+    data['id'] = data['release_id'].astype(str) + '.' + data['date'].astype(str)
+    # move the new id column to the front
+    data = data[['id'] + [col for col in data.columns if col != 'id']] 
+    print(data) 
+    db_user = os.getenv('DB_USER')
+    db_password = os.getenv('DB_PASSWORD')
+    
+    connection = mysql.connector.connect(
+        host="localhost",
+        user=db_user,
+        password=db_password,
+        database="my_database"
+    )
+
+    cursor = connection.cursor()
+
+    table_name = "economic_releases"
+    columns = ", ".join(data.columns) # assumes csv columns match db columns
+    placeholders = ", ".join(["%s"] * len(data.columns))
+    sql_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+
+    for index, row in data.iterrows():
+        values = tuple(row)  # convert row to tuple for insertion
+        cursor.execute(sql_query, values)
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
 
 
 if __name__ == "__main__":
