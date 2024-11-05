e5574033e5d975b7829f16276e0fde4ec2ba1f6c
Kyle Kauss <28989869+Kyle-sn@users.noreply.github.com>
2024-11-04 20:16:07 -0600

    more small fixes

--- shell/get_economic_releases.sh
+++ shell/get_economic_releases.sh
@@ -2,6 +2,5 @@
 
 DATEVAR=$(date +%Y%m%d)
 
-/usr/bin/python3 $HOME/PaperStreet/python/research/get_economic_releases.py \
->> /tmp/cron_logs/${DATEVAR}_economic_calendar.log 2>&1
+/usr/bin/python3 $HOME/PaperStreet/python/research/get_economic_releases.py /tmp/cron_logs/${DATEVAR}_economic_calendar.log 2>&1
 
